aae2dc011c98f0b454e91d19416b614cd48346ee
Jonash Gania <jonashtiglao@gmail.com>
2023-06-30 23:15:26 +0800

    Changed the game logic

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 let playerScore = 0;
 let computerScore = 0;
+const compScore = document.querySelector('.computer-score');
+const userScore = document.querySelector('.player-score');
+const rock = document.getElementById('rock');
+const paper = document.getElementById('paper');
+const scissor = document.getElementById('scissor');
+const roundResult = document.querySelector('.round-result-p');
+const modal = document.querySelector('.modal')
+const overlay = document.querySelector('.overlay');
+const modalText = document.querySelector('.modal-text');
+const playAgain = document.querySelector('.play-again');
 
 function getComputerChoice(){
     const randomMoves = ['rock', 'paper', 'scissor'];
@@ -7,45 +17,69 @@ function getComputerChoice(){
     return randomMoves[index];
 }
 
-function gameRound(playerSelection, computerSelection){
+function gameRound(playerSelection){
+    const computerSelection = getComputerChoice();
     
-    
-    if(playerSelection === 'rock' && computerSelection === 'paper'){
-        computerScore++
-        return "You Lose! Paper beats Rock!"
-    } else if (playerSelection === 'paper' && computerSelection === 'rock'){
+    if(playerSelection === computerSelection){
+        roundResult.textContent = "It's a tie!!"
+    }
+    if((playerSelection === 'rock' && computerSelection === 'scissor') || 
+       (playerSelection === 'paper' && computerSelection === 'rock') || 
+       (playerSelection === 'scissor' && computerSelection === 'paper')) {
         playerScore++
-        return "You Win! Paper beats Rock!"
-    } else if (playerSelection === 'paper' && computerSelection === 'scissor'){
+        userScore.textContent = playerScore;
+        compScore.textContent = computerScore;
+        roundResult.textContent = `You win! ${playerSelection} beats ${computerSelection}`
+       }
+    if((computerSelection === 'rock' && playerSelection === 'scissor') || 
+       (computerSelection === 'paper' && playerSelection === 'rock') || 
+       (computerSelection === 'scissor' && playerSelection === 'paper')) {
         computerScore++
-        return "You Lose! Scissor beats Paper"
-    } else if (playerSelection === 'scissor' && computerSelection === 'paper'){
-        playerScore++
-        return "You Win! Scissor beats Paper"
-    } else if (playerSelection === 'scissor' && computerSelection === 'rock'){
-        computerScore++
-        return "You Lose! Rock beats Scissor"
-    } else if (playerSelection === 'rock' && computerSelection === 'scissor'){
-        playerScore++
-        return "You Win! Rock beats Scissor"
-    } else {
-        return "Its a tie!"
+        compScore.textContent = computerScore;
+        userScore.textContent = playerScore;
+        roundResult.textContent = `You lose! ${computerSelection} beats ${playerSelection}`
+    }
+    if(playerScore === 5 || computerScore === 5){
+        endGame();
     }
 }
 
-function game(){
-    for(let i = 0; i < 5; i++){
-        const playerSelection = prompt("Enter your choice: ").toLowerCase();
-        computerSelection = getComputerChoice();
-        console.log(gameRound(playerSelection, computerSelection));
-        console.log("Player Score: " + playerScore);
-        console.log("Computer Score: " + computerScore);
-        
-        
-    }
 
-    console.log("Final Score:");
-    console.log("Player Score: " + playerScore);
-    console.log("Computer Score: " + computerScore);
+function clickEvent(){
+    rock.addEventListener('click', function(){
+        gameRound("rock");
+    })
+
+    paper.addEventListener('click', function(){
+        gameRound("paper");
+    })
+
+    scissor.addEventListener('click', function(){
+        gameRound("scissor");
+    })
+    playAgain.addEventListener('click', restartGame);
 }
-game();
+clickEvent();
+
+function endGame(){
+    if(playerScore === 5){
+        modal.style.visibility = 'visible';
+        overlay.style.visibility = 'visible';
+        modalText.textContent = "You won the game! Thank you for saving the humanity against computers.";
+    } else if (computerScore === 5){
+        modal.style.visibility = 'visible';
+        overlay.style.visibility = 'visible';
+        modalText.textContent = "You lost! Humanity is doomed...";
+    }
+}
+
+function restartGame(){
+    playerScore = 0;
+    computerScore = 0;
+    compScore.textContent = computerScore;
+    userScore.textContent = playerScore
+    modal.style.visibility = 'hidden';
+    overlay.style.visibility = 'hidden';
+    roundResult.textContent = "";
+}
+
